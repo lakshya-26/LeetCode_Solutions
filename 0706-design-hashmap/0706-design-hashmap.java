82a399608ba3bca69a07767396761b5378beebc8
Lakshya Kumar Singh <142081724+lakshya-26@users.noreply.github.com>
2023-10-04 23:21:35 +0530

    Time: 33 ms (20.19%), Space: 54.1 MB (32.70%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -2,9 +2,7 @@ class MyHashMap {
     int[] array;
     public MyHashMap() {
         this.array=new int[1000001];
-        for(int i=0;i<array.length;i++){
-            array[i]=-1;
-        }
+        Arrays.fill(array,-1);
     }
     
     public void put(int key, int value) {
