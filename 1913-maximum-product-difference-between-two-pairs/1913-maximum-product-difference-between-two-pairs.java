41c1b5a107c9c4bad79b7934d3e81b7559f0b87e
Lakshya Kumar Singh <142081724+lakshya-26@users.noreply.github.com>
2023-12-18 21:46:44 +0530

    Time: 8 ms (11.25%), Space: 43.8 MB (91.74%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
@@ -1,49 +1,8 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
-        int[] sortednums=quickSort(nums);
-        int n= sortednums.length;
-        int maxdiff = (sortednums[n-1]*sortednums[n-2])-(sortednums[0]*sortednums[1]);
+        Arrays.sort(nums);
+        int n= nums.length;
+        int maxdiff = (nums[n-1]*nums[n-2])-(nums[0]*nums[1]);
         return maxdiff;
-    }
-    public static int[] quickSort(int[] array) {
-        if (array == null || array.length == 0) {
-            return array;
-        }
-        sort(array, 0, array.length - 1);
-        return array;
-    }
-
-    private static void sort(int[] array, int low, int high) {
-        int i = low;
-        int j = high;
-        int pivot = array[low + (high - low) / 2];
-
-        while (i <= j) {
-            while (array[i] < pivot) {
-                i++;
-            }
-            while (array[j] > pivot) {
-                j--;
-            }
-
-            if (i <= j) {
-                swap(array, i, j);
-                i++;
-                j--;
-            }
-        }
-
-        if (low < j) {
-            sort(array, low, j);
-        }
-        if (i < high) {
-            sort(array, i, high);
-        }
-    }
-
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
     }
 }
