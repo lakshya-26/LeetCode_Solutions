6c3c74b86d99d9ed77679051903a417105147a98
Lakshya Kumar Singh <142081724+lakshya-26@users.noreply.github.com>
2024-02-02 22:45:42 +0530

    Time: 1124 ms (33.79%), Space: 0B (100.00%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,2 @@
+/* Write your PL/SQL query statement below */
+select query_name, round(avg(rating/position),2) quality,ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100, 2) poor_query_percentage from queries  group by query_name having query_name != 'null';
