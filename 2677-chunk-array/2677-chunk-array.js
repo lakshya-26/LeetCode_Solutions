8395fb6507e9cfc98103276c9676cff37dd70761
Lakshya Kumar Singh <142081724+lakshya-26@users.noreply.github.com>
2024-02-04 23:59:05 +0530

    Time: 52 ms (77.86%), Space: 51.8 MB (8.98%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -4,6 +4,14 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-    const newArr = require("lodash");
-    return newArr.chunk(arr,size);
-};
+  var chunkedArray = [];
+  var index = 0;
+
+  while (index < arr.length) {
+    chunkedArray.push(arr.slice(index, index + size));
+    index += size;
+  }
+
+  return chunkedArray;
+}
+
