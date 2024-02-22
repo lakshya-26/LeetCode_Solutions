be0652921001569689ed7997b98893508bb17c10
Lakshya Kumar Singh <142081724+lakshya-26@users.noreply.github.com>
2024-02-22 22:38:55 +0530

    Time: 1614 ms (26.52%), Space: 0B (100.00%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+SELECT employee_id, department_id FROM Employee WHERE 
+
+primary_flag = 'Y' UNION SELECT employee_id, department_id FROM 
+
+Employee WHERE employee_id IN (SELECT employee_id FROM Employee GROUP 
+
+BY employee_id HAVING COUNT(employee_id) = 1)
