9190de014277f5ed3b210cce2977a22756c8d875
Lakshya Kumar Singh <142081724+lakshya-26@users.noreply.github.com>
2024-05-06 22:49:57 +0530

    Time: 5 ms (54.41%), Space: 44.6 MB (24.54%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static String sortString(String str){
+    char c[] = str.toCharArray();
+    Arrays.sort(c);
+    return new String(c);
+  }
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        s = sortString(s);
+        t = sortString(t);
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) != t.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
