d87a4c49e83792de9386f0ce579d83e23f564b85
Lakshya Kumar Singh <142081724+lakshya-26@users.noreply.github.com>
2024-08-13 21:09:45 +0530

    Time: 3 ms (51.41%), Space: 45.5 MB (68.53%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int goal = nums.length - 1;
-
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (i + nums[i] >= goal) {
-                goal = i;
-            }
-        }
-
-        return goal == 0;        
+        int maxIndex = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxIndex) return false;
+            maxIndex = Math.max(maxIndex, i+nums[i]);
+        }     
+        return true;
     }
 }
